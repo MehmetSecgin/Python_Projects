0406db8e7ff240ab567eaac50e1f56e23b35f5b0
Mehmet Ali Seçgin <mehmetascgn@gmail.com>
2021-04-25 05:20:29 +0300

    Initial Commit

--- hw1/setup.py
+++ hw1/setup.py
@@ -4,23 +4,20 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mesecg", # Replace with your own username
+    name="circle-mesecg", # Replace with your own username
     version="0.0.1",
-    author="Mehmet Ali Seçgin",
+    author="Mehmet Ali Secgin",
     author_email="mesecg@ttu.ee",
     description="A package for HW1",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
-    },
+    url="https://github.com/Scentryum/Advanced_Python_Hw",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "circle"},
+    packages=setuptools.find_packages(where="circle"),
     python_requires=">=3.6",
 )
